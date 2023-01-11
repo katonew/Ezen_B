943857617f31384100d8f1cd24eface5184331d9
katonew <katonew98@gmail.com>
2023-01-11 16:40:17 +0900

    day09 과제2

--- frontend/public/JS/과제/과제2.js
+++ frontend/public/JS/과제/과제2.js
@@ -12,16 +12,16 @@ function onResult(){ //함수 s
 	//반복할때마다 초기화 되기 위해 선언당시에는 비우고 함수 시작시 단곤값 입력
 	table = '<tr><th>단</th><th>곱</th><th>값</th></tr>'
 	
-	//<input> 마크업을 js변수로 가져와 바로 대입
+	//<input> 단과 곱에 입력된 value 값 각 변수에 저장
 	let dan = document.querySelector('.dan').value
 	let gob = document.querySelector('.gob').value
 	
 //for문 i=1부터 입력된 곱까지, i * 입력된단을 sum에 대입한 후 table에 입력하는 것까지 반복
-	for(i=1; i<=곱; i++){	//for s
+	for(i=1; i<=gob; i++){	//for s
 		//곱셈 결과값 
-		sum = (단 * i);
+		sum = (dan * i);
 		//테이블행에 추가
-		table += '<tr><td>'+단+'</td><td>'+ i +'</td><td>'+sum+'</td></tr>'
+		table += '<tr><td>'+dan+'</td><td>'+ i +'</td><td>'+sum+'</td></tr>'
 	}	//for e
 	//html에 있는 'gu_table' 테이블에 추가
 	document.querySelector('.gu_table').innerHTML = table
