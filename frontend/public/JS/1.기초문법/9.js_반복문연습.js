b71c2aeedab71d92baecf9d069249014648bbe3d
katonew <katonew98@gmail.com>
2023-01-11 16:29:07 +0900

    day09 과제2번

--- frontend/public/JS/1.기초문법/9.js_반복문연습.js
+++ frontend/public/JS/1.기초문법/9.js_반복문연습.js
@@ -25,25 +25,75 @@ document.querySelector('body').innerHTML = output;
 console.log('-------------예4------------')
 let sum = 0;
 for(let i =1 ; i<=10 ; i++){
+	//console.log(" i =  " + i + "\t i<=10 : true \t" + sum + "+=" + i + "\t i++");
 	sum += i;
-	console.log(sum);
 }
+console.log(sum);
 //예5) 1부터 100까지의 7배수 누적합계
 console.log('-------------예5-1------------')
 sum = 0;
 for(let i = 7  ; i<=100 ; i+=7){
 	sum += i;
-	console.log('7배수 : '+i + ' 누적합계 : ' +sum);
 }
+console.log('누적합계 : ' +sum);
 
 console.log('-------------예5-2------------')
 sum = 0;
 for(let i =1 ; i<=100 ; i++ ){
 	if(i%7==0){
 		sum+=i
-		console.log('7배수 : '+i + ' 누적합계 : ' +sum);
 	}
 }
+console.log('누적합계 : ' +sum);
+
+// 예6) (2단)구구단
+console.log('-------------예6------------')
+output = ''
+for(let 곱=1; 곱<=9; 곱++){
+	// 곱은 1부터 9까지 1씩 증가하면서 반복처리
+	console.log("2 * "+ 곱+ " = " + (2*곱));
+	output += "2 * "+ 곱+ " = " + (2*곱) + "<br/>"
+}
+document.querySelector('body').innerHTML = output
+
+// 예7) 구구단
+console.log('-------------예7------------')
+// 1. 단 만들기
+for(let 단=2; 단<=9; 단++){	// [ 단이 1바퀴 돌때 곱이 9바퀴 ]
+	// 단 2부터 9까지 1씩 증가 반복처리
+	console.log(단 + "단");
+	for(let 곱 = 1; 곱<=9; 곱++){
+		// 곱 1부터 9까지 1씩 증가 반복처리
+		console.log(단 +" * "+ 곱+ " = " + (단*곱));
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
