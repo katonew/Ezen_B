15c7232e66f84107bd1dc1b4465bb224dc7dcc68
katonew <katonew98@gmail.com>
2023-01-16 09:30:57 +0900

    day11  과제4 합본

--- frontend/public/JS/과제/과제4/과제4.js
+++ frontend/public/JS/과제/과제4/과제4.js
@@ -1,53 +1,81 @@
-/*
-	주제 : 도서 관리 시스템 (1월 17일까지)
-	파일 : 과제4.html, 과제4.js, 과제4.css
-		* 배열 두개 : 
-					1. let = 도서목록[]
-					2. let = 대여목록[]
-		
-		1. 관리자 입장
-			-기능
-			1.도서 등록	[도서명]
-				1.중복검사
-				2.5~10글자 사이의 도서명만 가능	[띄어쓰기 포함]
-
-			2.도서 현황 출력
-				1.번호 2.도서명 3.도서대여여부 4.도서삭제[대여중에는 불가능]
-								
-			도서현황예시)
-				번호	도서				도서대여여부		비고
-				1	혼자공부하는자바		대여가능 or 대여중	삭제버튼
-				2	이것이자바다		대여가능 or 대여중	삭제버튼
-				2	C언어				대여가능 or 대여중	삭제버튼
-					
-		2. 고객 입장
-			-기능
-			1.도서 목록
-				1.번호 2.도서명 3.도서대여버튼 4.도서반납버튼
-				
-			도서목록예시)
-				번호	도서				도서대여여부		비고
-				1	혼자공부하는자바		대여가능 or 대여중	대여버튼/반납버튼
-				2	이것이자바다		대여가능 or 대여중	대여버튼/반납버튼
-				2	C언어				대여가능 or 대여중	대여버튼/반납버튼
-				
-			2.도서 대여
-				대여중인 도서 불가능
-				
-			3.도서 반납
-				대여중이 아닌 도서는 불가능
- */
-
- //도서입력부분은 내가 작성하는 부분이 아니기 때문에 확인을 위해 먼저 입력
- let 도서목록 = ['혼자공부하는자바','이것이자바다','C언어']
- //대여목록은 빈채로 선언
+ 
+ 
+ 
+ /* ---------공통부분 시작 ------------*/
+ // 도서목록 선언
+ let 도서목록 = []
+ //대여목록 선언
  let 대여목록 = []
- //반복되는 출력부분 output으로 선언
- let output = document.querySelector('.textBox')
- //첫 페이지 시작 때 출력함수 ( 없으면 테이블이 출력되지 않아 기능사용 불가)
- print()
+ /* ---------공통부분 끝--------------*/
  
+ /* ----------------최성아 작성 시작-------------- */
+ function click_btn() { // f s
+let content_register = document.querySelector('.content_register').value
+	등록하기( content_register ) // 이 안에 유효성 검사
+	printContent( ) // 이 안에 Delete 기능
+} // f e
+
+// 2. 삭제 버튼 함수
+
+function Delete ( i ){
+	for ( value of 도서목록 ){
+		if(대여목록.includes(도서목록[i]) == true){
+			alert ('대여중인 도서는 삭제가 불가능합니다.')
+			return;
+		}
+	}
+	도서목록.splice ( i , 1 )
+	printContent( )
+	print()
+}
+
+function printContent( ){
+	let html = `<tr>
+					<th>번호</th>
+					<th>도서</th>
+					<th>도서대여여부</th>
+					<th>비고</th>
+				</tr>`
+	for ( let i = 0 ; i < 도서목록.length; i++ ){ // for s
+			if( 대여목록.includes(도서목록[i]) == true ){
+			html += `<tr>
+					<td>${ i+1 }</td>
+					<td>${ 도서목록[i] }</td>
+					<td>대여중</td>
+					<td><button onclick="Delete( ${i} )">삭제</button></td>
+				</tr>`	
+			}else{
+			html += `<tr>
+					<td>${ i+1 }</td>
+					<td>${ 도서목록[i] }</td>
+					<td>대여가능</td>
+					<td><button onclick="Delete( ${i} )">삭제</button></td>
+				</tr>`	
+			}
+				document.querySelector('.result_box1').innerHTML = html
+	} // for e
+} // f e
+
+// 유효성검사 (중복, 글자수 제한)
+function 등록하기 ( 등록 ){
+	// 1. 중복검사/취소
+	if (도서목록.indexOf( 등록 ) >= 0 ) {
+		alert('도서명이 이미 등록되어있습니다. [등록되지 않습니다.]')
+		도서목록.splice( 도서목록.indexOf(등록), 1 )
+		printContent()
+		return;
+	}
+	if ( 등록.length < 5 || 등록.length > 10){
+		alert('글자 길이[5-10]를 맞춰주세요.')
+		printContent()
+		return;
+	}
+	도서목록.push( 등록 )
+	print()
+}
+ /* ----------------최성아 작성 끝-------------- */
  
+ /* ----------------권태형 작성 시작-------------- */
  function take(x){ //대여 함수
  	//도서목록에서 데이터 가져옴
  	let book = 도서목록[x]
@@ -57,14 +85,14 @@
  	let noneBook = 대여목록.indexOf(book)
  	//대여목록에 있으면 이미 대여된거
  	if(noneBook!=-1){
-		 output.innerHTML = '이미 대여된 책입니다.'
+		 alert('이미 대여된 책입니다.')
 		 return;
 	}
 	//대여목록에 없으면 대여실행 
  	else{
 		 //도서목록에 있는 데이터를 대여목록에 삽입
 		 대여목록.push(도서목록[x])
-		 output.innerHTML = '대여하였습니다.'
+		 alert('대여하였습니다.')
  	}
  	//출력함수
 	print()
@@ -79,13 +107,13 @@
  	let noneBook = 대여목록.indexOf(book)
  	//대여목록에 없으면 대여안한 책이라고 뜸
  	if(noneBook==-1){
-		 output.innerHTML = '대여하지 않은 책입니다.'
+		 alert('대여하지 않은 책입니다.')
 		 return;
 	}
 	//대여목록에 없으면 대여목록배열에서 삭제하고 반납되었습니다.
  	else{
 		 대여목록.splice(noneBook,1)
-		 output.innerHTML = '반납되었습니다.'
+		 alert('반납되었습니다.')
  	}
  	//출력함수
 	print()
@@ -94,7 +122,7 @@
  
  function print(){ // 출력함수
  	//초기 헤드라인
- 	let html = `<tr><th>번호</th><th>도서</th><th>대여여부</th><th>대여/반납</th></tr>`
+ 	let html = `<tr><th>번호</th><th>도서</th><th>도서대여여부</th><th>대여/반납</th></tr>`
 	// 내용추가
 	for(i=0;i<도서목록.length;i++){
 		// 첫째칸 = i+1 = 번호
@@ -124,7 +152,7 @@
 	 //대여목록에 있으면 대여중 return
 	 else{ return '대여중'}
  } // 대여 여부 함수 끝
- 
+ /* ----------------권태형 작성 끝-------------- */
  
  
  
