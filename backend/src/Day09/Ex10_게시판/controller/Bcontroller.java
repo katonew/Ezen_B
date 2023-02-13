6d2d2df19e7f7232e4fddfde82b6b808c5a187bc
katonew <katonew98@gmail.com>
2023-02-13 16:30:59 +0900

    day29 싱글톤 진행중

--- backend/src/Day09/Ex10_게시판/controller/Bcontroller.java
+++ backend/src/Day09/Ex10_게시판/controller/Bcontroller.java
@@ -1,11 +1,32 @@
 package Day09.Ex10_게시판.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+import Day09.Ex10_게시판.model.Board;
+
+
 public class Bcontroller {
-	
+	private ArrayList<Board> BoardDb= new ArrayList<>();
+			
 	// 1. 싱글톤 객체
 	private static Bcontroller bc = new Bcontroller();
 	private  Bcontroller() {}
+	
+	
 	public static Bcontroller getInstance() {
 		return bc;
+	}
+	
+	//2. 쓰기 함수
+	public boolean write(String title, String content, String writer, String password, Date date,int view) {
+		//1. 유효성 검사 [생략]
+		//2. 저장[DB대신 리스트]
+			//1.객체화[	]
+		Board board = new Board(title,content,writer,password,date,view);
+			//2. 객체를 리스트 담기
+		BoardDb.add(board);
+		//3. 결과 반환
+		return false;
 	}
 }
--- backend/src/Day09/Ex10_게시판/view/Front.java
+++ backend/src/Day09/Ex10_게시판/view/Front.java
@@ -1,6 +1,9 @@
 package Day09.Ex10_게시판.view;
 
+import java.util.Date;
 import java.util.Scanner;
+
+import Day09.Ex10_게시판.controller.Bcontroller;
 
 public class Front {
 	// 1. 싱글톤 객체	[1. 프로그램 내 하나의 객체 - 공유 메모리]
@@ -11,21 +14,35 @@ public class Front {
 		}
 		
 		
-		// 필드 : 입력 객체 필드
+		// 필드 : 입력 객체 필드 [ 인스턴스 멤버]
 		private Scanner scanner = new Scanner(System.in);
 		
 		//2.index 함수
 		public void index() {
 			while(true) {
+				print_page(); //출력함수
 				System.out.print("1. 쓰기 : ");
 				int ch = scanner.nextInt();
-				if(ch==1) {write_page();}
+				if(ch==1) {write_page();}	//쓰기 함수
 			} // while e
 		} // index 함수 e
 		
 		//3. 쓰기 페이지 함수
 		private void write_page() {
 			System.out.println("----------글쓰기 페이지------------");
+			System.out.print("제목 : ");
+			String title = scanner.next();
+			System.out.print("내용 : ");
+			String content = scanner.next();
+			System.out.print("작성자 : ");
+			String writer = scanner.next();
+			System.out.print("비밀번호 : ");
+			String password = scanner.next();
+			Date date = new Date();
+			System.out.println("현재날짜/시간 : "+date);
+			int view = 0;
+			Bcontroller.getInstance().write(title, content,writer,password,date,view);
+			
 		}
 		
 		//4. 출력 페이지 함수
