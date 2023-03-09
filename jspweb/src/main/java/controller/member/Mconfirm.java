0cb47d06c6425545bbff7c8e4e80bd445f75e685
katonew <katonew98@gmail.com>
2023-03-09 17:13:50 +0900

    03/09 mconfirm 수정

---
+++ jspweb/src/main/java/controller/member/Mconfirm.java
@@ -0,0 +1,47 @@
+package controller.member;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.dao.MemberDao;
+
+/**
+ * Servlet implementation class Mconfirm
+ */
+@WebServlet("/mconfirm")
+public class Mconfirm extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Mconfirm() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 검사할 아이디 요청
+		String mid =request.getParameter("mid");
+		// 2. DAO 에게 전달하고 결과 받기
+		boolean result = MemberDao.getInstance().idcheck(mid);
+		// 3. 결과 응답
+		response.getWriter().print(result);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
